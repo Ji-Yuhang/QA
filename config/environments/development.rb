14b3d9d94ef11c67c1522e4a4bfe4ba7740365b0
Ji-Yuhang <yuhang.silence@gmail.com>
2016-07-02 19:10:12 +0800

    add user model

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -15,7 +15,19 @@ Rails.application.configure do
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.delivery_method =:smtp
 
+  config.action_mailer.smtp_settings = {
+    address: "smtp.mxhichina.com",
+    port: 465,
+    domain: "ibuluo.me",
+    authentication: "plain",
+    enable_starttls_auto: true,
+    user_name: "support@ibuluo.me",
+    password: "1qaz@WSX"
+  }
+
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
