98aab4f14803f7fc0fb3f9912535c86d1b1b89b9
Ji-Yuhang <yuhang.silence@gmail.com>
2016-07-05 00:36:03 +0800

    .

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,20 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   has_many :units
+
+  def qas
+    qalist
+  end
+
+  def qas=(s)
+  end
+
+  def add_pair(q,a)
+    return false if qaplist.include? q.id
+    qaplist[q.id] = a.id
+  end
+
+  def qaplist
+    @data ||= {3=>4,2=>1}
+  end
 end
